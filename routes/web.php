98422f166c03d180278e7c7dcf88dc82a86d194d
FriasIvan <frias.ivn@gmail.com>
2025-04-05 01:37:52 +0800

    Create Skeleton Faculty Page

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,81 @@ Route::get('/', function () {
 Route::get('/exams', function(){
     return view('exams/index');
 });
+Route::get('/exams/create', function(){
+    return view('exams/create');
+});
+Route::get('/exams/show', function(){
+    return view('exams/show');
+});
+Route::get('/exams/edit', function(){
+    return view('exams/show');
+});
+
+
+Route::get('/questions', function(){
+    return view('questions/index');
+});
+Route::get('/questions/create', function(){
+    return view('questions/create');
+});
+Route::get('/questions/show', function(){
+    return view('questions/show');
+});
+Route::get('/questions/edit', function(){
+    return view('questions/show');
+});
+
+
+Route::get('/topics', function(){
+    return view('topics/index');
+});
+Route::get('/topics/create', function(){
+    return view('topics/create');
+});
+Route::get('/topics/show', function(){
+    return view('topics/show');
+});
+Route::get('/topics/edit', function(){
+    return view('topics/show');
+});
+
+
+Route::get('/subjects', function(){
+    return view('subjects/index');
+});
+Route::get('/subjects/create', function(){
+    return view('subjects/create');
+});
+Route::get('/subjects/show', function(){
+    return view('subjects/show');
+});
+Route::get('/subjects/edit', function(){
+    return view('subjects/show');
+});
+
 Route::get('/exams/hello/time/set', function(){
     return view('exams/index');
 });
+
+Route::get('/reports', function(){
+    return view('reports');
+});
+
+Route::get('/notifications', function(){
+    return view('notifications');
+});
+
+Route::get('/settings', function(){
+    return view('settings');
+});
+
+Route::get('/profile', function(){
+    return view('profile');
+});
+
+
+
+
 // Route::get('/jobs/create', [JobController::class, 'create']);
 // Route::post('/jobs', [JobController::class, 'store']);
 // Route::get('/jobs/{job}', [JobController::class, 'show']);
