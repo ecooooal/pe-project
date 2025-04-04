14fb7ea1598813fe1d358eadefb2722dc9414b03
FriasIvan <frias.ivn@gmail.com>
2025-04-04 20:11:10 +0800

    Create Breadcrumb Links

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/exams', function(){
+    return view('exams/index');
+});
+Route::get('/exams/hello/time/set', function(){
+    return view('exams/index');
+});
+// Route::get('/jobs/create', [JobController::class, 'create']);
+// Route::post('/jobs', [JobController::class, 'store']);
+// Route::get('/jobs/{job}', [JobController::class, 'show']);
+// Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])->middleware(['auth', 'can:edit-job,job']);
+// Route::patch('/jobs/{job}', [JobController::class, 'update']);
+// Route::delete('/jobs/{job}', [JobController::class, 'delete']);
