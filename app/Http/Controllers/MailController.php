12bf847fae5cebb398c64a2d8222ac8980b7879f
Mark Laurence <mlnepomuceno@pcu.edu.ph>
2025-06-05 20:31:30 +0800

    a

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\TestMail;
+use App\Mail\TestMail;//
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
