9e172e7023808d55f698821f1dc4396f695c3a86
FriasIvan <frias.ivn@gmail.com>
2025-08-27 03:27:23 +0800

    Add fetching and display exam dashboard data

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -6,7 +6,9 @@ use App\Models\Course;
 use App\Models\Exam;
 use App\Models\User;
 use App\Services\UserService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class LandingPageController extends Controller
 {
@@ -36,9 +38,60 @@ class LandingPageController extends Controller
     }
 
     public function examReportShow(){
-        $exam = Exam::count();
+        $exam_dashboard_data = Http::timeout(30)->get('http://fastapi:80/api/dashboard/load-exam')->json();
+        $examination_dates = collect($exam_dashboard_data['examination_dates'])
+            ->map(function ($row) {
+                return [
+                    'id'   => $row[0],
+                    'name' => $row[1],
+                    'date' => $row[2],
+                ];
+            })->values()->toArray();
 
-        return view('components/graphs/homepage-exam', ['exam' =>$exam]);
+        $grouped = collect($examination_dates)    
+            ->groupBy('date')
+            ->map(function ($items) {
+                return [
+                    'names' => $items->pluck('name')->all(),
+                    'count' => $items->count(),
+                ];
+            });
+
+        $question_exams = collect($exam_dashboard_data['question_exams'])->map(function ($row) {
+            try {
+                $formatted_date = Carbon::parse($row[3])->format('F j, Y');
+            } catch (\Exception $e) {
+                $formatted_date = 'No Examination Date';
+            }
+
+            return [
+                'id'   => $row[0],
+                'name' => $row[1],
+                'is_published' => $row[2],
+                'examination_date' => $formatted_date,
+                'question_count' => $row[4]
+            ];
+        })->values()->toJson();
+
+        $exam_courses = collect($exam_dashboard_data['exam_courses'])->map(function ($row) {
+            return [
+                'id'   => $row[0],
+                'name' => $row[1],
+                'abbreviation' => $row[2],
+                'exam_count' => $row[3]
+            ];
+        })->values()->toJson();
+        $exam = [
+            'count' => $exam_dashboard_data['exam_count'],
+            'published_count' => $exam_dashboard_data['published_count'],
+            'unpublished_count' => $exam_dashboard_data['unpublished_count'],
+            'examination_dates' => $grouped,
+            'question_exams' => $question_exams,
+            'exam_courses' => $exam_courses,
+            'exams' => $exam_dashboard_data
+        ];
+        
+        return view('components/graphs/homepage-exam', $exam);
     }
     public function courseReportShow(){
         $user = auth()->user();
