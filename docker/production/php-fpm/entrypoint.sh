a4745fe7a9dcdca1631572a076f03947f4f5bcf9
FriasIvan <frias.ivn@gmail.com>
2025-11-26 23:12:37 +0800

    Update entrypoints for phpfpm, Update app.css for twig files

--- docker/production/php-fpm/entrypoint.sh
+++ docker/production/php-fpm/entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 set -e
 
+echo "Clearing and caching config..."
+php artisan config:clear
+php artisan config:cache
+php artisan route:clear
+php artisan view:clear
+
 # Initialize storage directory if empty
 # -----------------------------------------------------------
 # If the storage directory is empty, copy the initial contents
 # and set the correct permissions.
 # -----------------------------------------------------------
+echo "Setting permissions for storage and bootstrap/cache directories..."
 if [ ! "$(ls -A /var/www/storage)" ]; then
   echo "Initializing storage directory..."
   cp -R /var/www/storage-init/. /var/www/storage
@@ -27,14 +34,21 @@ rm -rf /var/www/storage-init
 # -----------------------------------------------------------
 # Ensure the database schema is up to date.
 # -----------------------------------------------------------
-php artisan migrate --force
 
-# Clear and cache configurations
-# -----------------------------------------------------------
-# Improves performance by caching config and routes.
-# -----------------------------------------------------------
-php artisan config:cache
-php artisan route:cache
+check_migration_status() {
+    php artisan migrate:status > /dev/null 2>&1
+    return $?
+}
+echo "Checking migration status..."
+check_migration_status
+if [ $? -eq 0 ]; then
+    echo "Migrations table exists. Skipping migration."
+else
+    echo "Migrations table does not exist or there is an issue. Running migrations."
+    php artisan migrate --force
+fi
+
+echo "Starting PHP-FPM..."
 
 # Run the default command
 exec "$@"
